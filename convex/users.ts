392e5062f1369074f95c862f13c62188555d1d9c
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-07-06 21:45:00 +0530

    Home page Theme selector completed

--- convex/users.ts
+++ convex/users.ts
@@ -25,20 +25,22 @@ export const syncUser = mutation({
   },
 });
 
-// export const getUser = query({
-//   args: { userId: v.string() },
+export const getUser = query({
+  args: { userId: v.string() },
 
-//   handler: async (ctx, args) => {
-//     if (!args.userId) return null;
+  handler: async (ctx, args) => {
+    if (!args.userId) return null;
 
-//     const user = await ctx.db 
-//       .query("users")
-//       .withIndex("by_user_id")
-//       .filter((q) => q.eq(q.field("userId"), args.userId))
-//       .first();
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .first();
 
-//     if (!user) return null;
+    if (!user) return null;
 
-//     return user;
-//   },
-// });
+    return user;
+  },
+});
+
+
---
+++ src/store/useCodeEditorStore.ts
@@ -0,0 +1,79 @@
+import { CodeEditorState } from "./../types/index";
+import { LANGUAGE_CONFIG } from "@/app/(root)/_constants";
+import { create } from "zustand";
+import { editor as MonacoEditor } from "monaco-editor";
+
+const getInitialState = () => {
+  // if we're on the server, return default values
+  if (typeof window === "undefined") {
+    return {
+      language: "javascript",
+      fontSize: 16,
+      theme: "vs-dark",
+    };
+  }
+
+  // if we're on the client, return values from local storage bc localStorage is a browser API.
+  const savedLanguage = localStorage.getItem("editor-language") || "javascript";
+  const savedTheme = localStorage.getItem("editor-theme") || "vs-dark";
+  const savedFontSize = localStorage.getItem("editor-font-size") || 16;
+
+  return {
+    language: savedLanguage,
+    theme: savedTheme,
+    fontSize: Number(savedFontSize),
+  };
+};
+
+export const useCodeEditorStore = create<CodeEditorState>((set, get) => {
+  const initialState = getInitialState();
+
+  return {
+    ...initialState,
+    output: "",
+    isRunning: false,
+    error: null,
+    editor: null,
+    executionResult: null,
+
+    getCode: () => get().editor?.getValue() || "",
+
+    setEditor: (editor: MonacoEditor.IStandaloneCodeEditor) => {
+      const savedCode = localStorage.getItem(`editor-code-${get().language}`);
+      if (savedCode) editor.setValue(savedCode);
+
+      set({ editor });
+    },
+
+    setTheme: (theme: string) => {
+      localStorage.setItem("editor-theme", theme);
+      set({ theme });
+    },
+
+    setFontSize: (fontSize: number) => {
+      localStorage.setItem("editor-font-size", fontSize.toString());
+      set({ fontSize });
+    },
+
+    setLanguage: (language: string) => {
+      // Save current language code before switching
+      const currentCode = get().editor?.getValue();
+      if (currentCode) {
+        localStorage.setItem(`editor-code-${get().language}`, currentCode);
+      }
+
+      localStorage.setItem("editor-language", language);
+
+      set({
+        language,
+        output: "",
+        error: null,
+      });
+    },
+
+    runCode: async () => {
+      //todo
+    }
+  };
+});
+
